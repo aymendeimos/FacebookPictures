a67e158decbc61567018b187b01a821215c367ef
Aymen Deimos <aymengraoui@gmail.com>
2017-11-20 16:16:05 +0000

    Add navbar to the notfound component

--- react-frontend/src/components/Notfound.js
+++ react-frontend/src/components/Notfound.js
@@ -1,10 +1,13 @@
 import React from "react";
 
+import Navbar from "./navbar";
+
 class Notfound extends React.Component {
 
     render() {
         return (
             <div>
+                <Navbar />
                 <div id="notfound"></div>
             </div>
         );
