68f12b9cd2dd24ab82f406a72286ad464d638aaf
Aymen Deimos <aymengraoui@gmail.com>
2017-11-21 00:48:01 +0000

    Change the first page to be dynamie
      - the first page now show login and sign up  buttons if there's no user logged in if its not it show a button with the name of the current user and link to the profile

--- react-frontend/src/components/Firstpage.js
+++ react-frontend/src/components/Firstpage.js
@@ -1,8 +1,46 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import axios from "axios";
 
 class Firstpage extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            email: '',
+            facebook_name: '',
+            logged : true
+        }
+        this.loadAccount();
+    }
+
+    loadAccount() {
+        let self = this;
+        axios.post("http://localhost:4200/users/gettoken")
+            .then(res => {
+                if (res.data.success) {
+                    self.setState({
+                        email: res.data.token.email,
+                        facebook_name: res.data.token.facebook_name,
+                        logged : true,
+                    });
+                } else {
+                    self.setState({
+                        logged : false,
+                    });
+                }
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }
+
+
     render() {
+        let username = (this.state.facebook_name ? this.state.facebook_name : this.state.email);
+        let login = (<Link to="/user-login" key="0"><button className="btn btn-lg waves-effect waves-light elegant-color-dark" type="" id="SignUp">LogIn</button></Link>);
+        let singnup = (<Link to="/user-registration" key="1"><button className="btn btn-lg waves-effect waves-light elegant-color-dark" type="" id="SignUp">SignUp</button></Link>);
+        let profile = (<Link to="/user-profile"><button className="btn btn-lg waves-effect waves-light elegant-color-dark" type="" id="">{username}</button></Link>);
+        let control = (this.state.logged?profile:[login ,singnup]);
         return (
             <div className="index container animated fadeIn">
                 <header className="masthead">
@@ -16,21 +54,7 @@ class Firstpage extends React.Component {
                                     <div className="container">
                                         <div className="col-lg-8 mx-auto">
                                             <div className="text-center">
-                                                <Link to="/user-login">
-                                                    <button
-                                                        className="btn btn-lg waves-effect waves-light elegant-color-dark"
-                                                        type=""
-                                                        id="SignUp">LogIn
-                                                    </button>
-                                                </Link>
-
-                                                <Link to="/user-registration">
-                                                    <button
-                                                        className="btn btn-lg waves-effect waves-light elegant-color-dark"
-                                                        type=""
-                                                        id="SignUp">SignUp
-                                                    </button>
-                                                </Link>
+                                                {control}
                                             </div>
                                         </div>
                                     </div>
